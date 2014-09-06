882f625ea7969ebb85e967260034328e688cbf4f
Tyrantz <raymanv3@gmail.com>
2014-09-06 19:09:35 +0000

    adds getMirrors and getHostingSite methods to bestanime.py

--- src/plugins/bestanime.py
+++ src/plugins/bestanime.py
@@ -1,17 +1,22 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
+import re
 
 USER_AGENT = {'User-agent': 'Mozilla/5.0'}
+
 
 def searchable_string(user_input):
     return user_input.replace(' ', '+')
 
+
 def search_page(anime_name):
-    website_request = Request("http://bestanimes.tv/?s=" + searchable_string(anime_name), None, USER_AGENT)
+    website_request = Request("http://bestanimes.tv/?s=" +
+                              searchable_string(anime_name), None, USER_AGENT)
     website_html = urlopen(website_request).read()
     return website_html
+
 
 def get_episodes(html_text):
     soup = BeautifulSoup(html_text)
@@ -19,10 +24,47 @@ def get_episodes(html_text):
     for i in episode_list:
         print(i.getText().strip())
 
+
 def get_episode_url(html_text):
+    li = []
     soup = BeautifulSoup(html_text)
     episode_url_list = soup.findAll('div', {'class': 'episode-list'})
     for i in episode_url_list:
         episode_url = i.findAll('a')
         for f in episode_url:
-            print(f['href'])
+            li.append(f['href'])
+    return li
+
+
+def getMirrors(episodeURI):
+    """
+    Pass it a url and it will return a list with the urls to the
+    mirrors of that particular url passed. Will do this for the first element
+    in the link passed to it (hard coded as of now will change in future)
+    """
+    li = []
+    episodeRequest = Request(episodeURI, None, USER_AGENT)
+    episodeHtml = urlopen(episodeRequest).read()
+    soup = BeautifulSoup(episodeHtml)
+    mirrorList = soup.findAll('div', {'class', 'sources'})
+    for i in mirrorList:
+        mirrorUrl = i.findAll('a')
+        for f in mirrorUrl:
+            li.append(f['href'])
+    return li
+
+
+def getHostingSite(mirror):
+    """
+    pass it the url of a mirror and it will return the video host provider
+    """
+    host = ""
+    hostRequest = Request(mirror, None, USER_AGENT)
+    hostHtml = urlopen(hostRequest)
+    soup = BeautifulSoup(hostHtml)
+    hostParse = soup.findAll('div', {'id': 'default', 'class': 'default'})
+    for i in hostParse:
+        tmp = i.findAll('iframe')
+        for f in tmp:
+            host = f['src']
+    return host

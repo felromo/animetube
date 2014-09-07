e80d6ff3cb1ee7ae4c6d2cda2918bd5bc2bc144f
Tyrantz <raymanv3@gmail.com>
2014-09-07 16:15:56 +0000

    adds mp4upload parser

---
+++ src/plugins/mp4upload.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import re
+from urllib.request import urlopen, Request
+from urllib.parse import unquote
+from bs4 import BeautifulSoup
+
+USER_AGENT = {'User-agent': 'Mozilla/5.0'}
+
+
+def mp4upload(hostUrl):
+    """
+    takes in a link with the embeded video player and returns a clean url of
+    the actual content to be passed onto the player
+    """
+    content = ""
+    pattern = r'\'file\':\s*\'(.+)\''
+    videoRequest = Request(hostUrl, None, USER_AGENT)
+    videoHtml = urlopen(videoRequest).read()
+    soup = BeautifulSoup(videoHtml)
+    scripts = soup.findAll('script', {'type': 'text/javascript'})
+    script = scripts[3]
+    for line in script:
+        match = re.findall(pattern, line)
+        content = match
+    return unquote(content[0])

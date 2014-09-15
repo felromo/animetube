d5b484dbcb78b9cb9c66f4fae09c157bb7c27558
Tyrantz <raymanv3@gmail.com>
2014-09-15 22:56:02 +0000

    Adds main2.py which is the new front end for animetube that uses parser as a class instead of a function. also adds parser.py and the __init__.py that was missing in the plugins folder

---
+++ src/main2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+from plugins import parser
+from subprocess import call, Popen, PIPE, STDOUT
+
+try:
+    from subprocess import DEVNULL
+except ImportError:
+    import os
+    DEVNULL = open(os.devnull, 'wb')
+
+
+def menu():
+    print ("[N]ext Episode")
+    print ("[P]revious Episode")
+    print ("[D]ifferent Anime")
+    print ("[R]eplay same Episode")
+    print ("[Q]uit out of program")
+
+
+def player(videoContent):
+    call(['vlc', videoContent, '--play-and-exit'], stdout=DEVNULL,
+         stderr=STDOUT)
+
+if __name__ == '__main__':
+    parser = parser.parser()
+    mainLoop = 'd'
+    content = ""
+    while mainLoop != 'q':
+        valid = True
+        if mainLoop == 'd':
+            animeName = input("Anime Name: ")
+            parser.setAnime(animeName)
+            for episode in parser.getEpisodes():
+                print (episode)
+            episodeChoice = input("Choose an episode id (number in brackets []): ")
+            content = parser.playEpisode(int(episodeChoice))
+        elif mainLoop == 'n':
+            content = parser.playEpisode(parser.nextEpisode)
+        elif mainLoop == 'p':
+            content = parser.playEpisode(parser.prevEpisode)
+        elif mainLoop == 'r':
+            pass  # don't need to do anything to replay
+        else:
+            print ("Not a valid option")
+            valid = False
+        if valid:
+            player(content)
+        menu()
+        mainLoop = input(">>")
---
+++ src/plugins/parser.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+from plugins import bestanime, trollvideo
+
+
+class parser():
+
+
+    def __init__(self, animeName="", action='d', siteParser='bestanime',
+                 hostParser='trollvideo'):
+        self.animeName = animeName
+        self.episodes = None
+        self.episodeUrls = None
+        self.prevEpisode = None
+        self.nextEpisode = None
+        self.mirrors = None
+        self.host = None
+
+
+    def __setEpisodes(self):
+        self.episodes = bestanime.get_episodes(
+            bestanime.search_page(self.animeName))
+
+    def __setEpisodeUrls(self):
+        self.episodeUrls = bestanime.get_episode_url(
+            bestanime.search_page(self.animeName))
+
+    def __setMirrors(self, episodeChoice):
+        if type(episodeChoice) is int:
+            self.mirrors = bestanime.getMirrors(self.episodeUrls[episodeChoice])
+        else:
+            self.mirrors = bestanime.getMirrors(episodeChoice)
+
+    def __setHost(self):
+        # this should change according to a future config file
+        self.host = bestanime.getHostingSite(self.mirrors[0])
+
+    def __setNextPrev(self, episodeChoice):
+        if type(episodeChoice) is int:
+            self.prevEpisode, self.nextEpisode = bestanime.getNextPrev(
+                self.episodeUrls[episodeChoice])
+        else:
+            self.prevEpisode, self.nextEpisode = bestanime.getNextPrev(
+                episodeChoice)
+
+    def setAnime(self, animeName):
+        self.animeName = bestanime.searchable_string(animeName)
+        self.__setEpisodes()
+        self.__setEpisodeUrls()
+
+    def getEpisodes(self):
+        li = []
+        selector = 0
+        for episode in self.episodes:
+            li.append("([{}] {})".format(selector, episode))
+            selector = selector + 1
+        return li
+
+    def playEpisode(self, episodeChoice, selection=''):
+        self.__setNextPrev(episodeChoice)
+        self.__setMirrors(episodeChoice)
+        self.__setHost()
+        return trollvideo.trollvideo(self.host)
+
+
+
+
+
+

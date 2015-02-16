0d4e939a333516634551c292bbaa799eee6ffd96
Tyrantz <raymanv3@gmail.com>
2015-02-16 05:02:04 +0000

    adds playbb and yucache host-level parsers

---
+++ src/plugins/playbb.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import re
+from urllib.request import urlopen, Request
+from urllib.parse import unquote
+from bs4 import BeautifulSoup
+
+USER_AGENT = {'User-agent': 'Mozilla/5.0'}
+
+
+def playbb(hostUrl):
+    """
+    takes in a link with the embeded video player and returns a clean url of
+    the actual content to be passed onto the player
+    """
+    content = ""
+    search_pattern = r'_url = "(.+)"'
+    videoRequest = Request(hostUrl, None, USER_AGENT)
+    videoHtml = urlopen(videoRequest).read()
+    soup = BeautifulSoup(videoHtml)
+    scripts = soup.findAll('script', {'type': 'text/javascript'})
+    script = scripts[-1]
+    for line in script:
+        match = re.search(search_pattern, line)
+        content = (unquote(match.group(1)))
+    return content

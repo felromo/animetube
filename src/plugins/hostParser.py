6dcbbc15111d3915d81397c5b73408a0961dd44f
Tyrantz <raymanv3@gmail.com>
2015-02-17 04:18:26 +0000

    adds all the new host-level parsers to the list of useable plugins

--- src/plugins/hostParser.py
+++ src/plugins/hostParser.py
@@ -14,14 +14,32 @@ def hostParser(url):
     if finalMatch == "auengine":
         from plugins import auengine
         content = auengine.auengine(url)
-    if finalMatch == "trollvid":
-        from plugins import trollvideo
-        content = trollvideo.trollvideo(url)
+    if finalMatch == "easyvideo":
+        from plugins import easyvideo
+        content = easyvideo.easyvideo(url)
     if finalMatch == "mp4upload":
         from plugins import mp4upload
         content = mp4upload.mp4upload(url)
+    if finalMatch == "playbb":
+        from plugins import playbb
+        content = playbb.playbb(url)
+    if finalMatch == "trollvid":
+        from plugins import trollvideo
+        content = trollvideo.trollvideo(url)
+    if finalMatch == "video44":
+        from plugins import video44
+        content = video44.video44(url)
     if finalMatch == "videofun":
         from plugins import videofun
         content = videofun.videofun(url)
+    if finalMatch == "videowing":
+        from plugins import videowing
+        content = videowing.videowing(url)
+    if finalMatch == "vidzur":
+        from plugins import vidzur
+        content = vidzur.vidzur(url)
+    if finalMatch == "yucache":
+        from plugins import yucache
+        content = yucache.yucache(url)
 
     return content

8d1bf72c77b81cf989ecd5ab52916dda76ec7322
Tyrantz <raymanv3@gmail.com>
2015-03-04 23:51:23 +0000

    Fixes #7

--- src/plugins/trollvideo.py
+++ src/plugins/trollvideo.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen, Request
 from urllib.parse import unquote
 from bs4 import BeautifulSoup
 
-USER_AGENT = {'User-agent': 'Mozilla/5.0'}
+USER_AGENT = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.22280 Safari/537.36'}
 
 
 def trollvideo(hostUrl):
@@ -12,14 +12,16 @@ def trollvideo(hostUrl):
     takes in a link with the embeded video player and returns a clean url of
     the actual content to be passed onto the player
     """
+
     content = ""
     search_pattern = r'file:\s(.+)'
     videoRequest = Request(hostUrl, None, USER_AGENT)
     videoHtml = urlopen(videoRequest).read()
     soup = BeautifulSoup(videoHtml)
     scripts = soup.findAll('script', {'type': 'text/javascript'})
-    script = scripts[-1]
+    script = scripts[-2]
     for line in script:
         match = re.search(search_pattern, line)
         content = match.group(1)
     return unquote(content[1:-2])
+

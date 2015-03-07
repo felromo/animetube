e6328b6a4d8e407b23def5ec2a4746f6c417b4a2
Tyrantz <raymanv3@gmail.com>
2015-03-07 18:17:36 +0000

    removes old lines of code that were commented out, renames function in testing run, adds better comments and removes old ones

--- src/plugins/hostParser.py
+++ src/plugins/hostParser.py
@@ -15,18 +15,20 @@ def hostParser(url):
     for _, _, plugins in walk('./HostParsers'):
         for plugin in plugins:
             plugin_list.append(splitext(plugin)[0])
-            # break # skip over pyc files with the same name
 
     # this runs through all the modules in the dir and imports the url match
     for plugin in plugin_list:
-        if url in plugin and not 'cpython' in plugin:
+        if url in plugin and 'cpython' not in plugin:
             tmp = plugin
             parser_plugin = __import__('HostParsers.' + plugin, globals(),
                                        locals(), [plugin])
-    # this needs to call a function from inside the imported module ;-;
+    # this iterates through key, val of the vars() dict for parser_plugin
+    # when key matches our wanted plugin use its val as a function
+    # and return that functions return
     for key, val in vars(parser_plugin).items():
-        if tmp == key: # this is not working (plugin is empty)
+        if tmp == key:
             return val(url)
 
+# for testing purposes only runs if this is executed and not imported
 if __name__ == '__main__':
-    hostParser2('auengine')
+    hostParser('auengine')
